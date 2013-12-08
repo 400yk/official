7381b711a5aab16c885017a4be0973ec03b902fe
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-08 12:46:06 -0800

    Finish part 2 of the tutorial: customize admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Choice, Poll
 
-admin.site.register(Poll)
+#class PollAdmin(admin.ModelAdmin):
+#    fields = ['pub_date', 'question']
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline): # Use TabularInline to save screen space)
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('None',    {'fields': ['question']}),
+            ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]       
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+#admin.site.register(Choice)
