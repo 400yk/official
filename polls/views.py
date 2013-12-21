a18bfa404c04ca75754d71293be7e572ec3dd1d0
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2013-12-21 12:24:56 -0800

    Add tests

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
@@ -11,11 +12,17 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        return Poll.objects.filter(
+               pub_date__lte = timezone.now()
+               ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # excludes any polls that aren't published yet
+        return Poll.objects.filter(pub_date__lte = timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Poll
